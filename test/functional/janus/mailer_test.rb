350eb1945e6a37e98314783c818adfbc42c27815
Julien Portalier <ysbaddaden@gmail.com>
2011-04-10 15:01:01 +0200

    Fixed a test bug for Janus::Mailer.send_reset_password_instructions.

--- test/functional/janus/mailer_test.rb
+++ test/functional/janus/mailer_test.rb
@@ -4,9 +4,11 @@ class Janus::MailerTest < ActionMailer::TestCase
   test "reset_password_instructions" do
     users(:julien).generate_reset_password_token!
     
-    mail = JanusMailer.reset_password_instructions(users(:julien))
+    mail = JanusMailer.reset_password_instructions(users(:julien)).deliver
     assert_equal [users(:julien).email], mail.to
     assert !mail.subject.blank?
-    assert_match Regexp.new("a[href=" + edit_user_password_url(:token => users(:julien).reset_password_token) + "]"), mail.body
+    
+    url = edit_user_password_url(:token => users(:julien).reset_password_token)
+    assert_match Regexp.new(Regexp.escape(url)), mail.encoded
   end
 end
