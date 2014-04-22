0d13d631ac3c80e6f5404651c4860f01ce12dd86
Julien Portalier <julien@portalier.com>
2014-04-22 13:53:15 +0200

    Fixed test suite on Rails 4.0

--- test/rails_app/config/initializers/secret_token.rb
+++ test/rails_app/config/initializers/secret_token.rb
@@ -1,3 +1,7 @@
-unless RailsApp::Application.config.respond_to?(:secret_key_base)
+if RailsApp::Application.config.respond_to?(:secret_key_base)
+  if Rails.version < '4.1.0'
+    RailsApp::Application.config.secret_key_base = 'c6a67697877c66be70cdcc4680f37593045a721cf757de4110f9749877cb32f94fe4ddaa5e816af4555d91c4f6142a401972474d50fe620d41ede300d3143d4a'
+  end
+else
   RailsApp::Application.config.secret_token = 'c6a67697877c66be70cdcc4680f37593045a721cf757de4110f9749877cb32f94fe4ddaa5e816af4555d91c4f6142a401972474d50fe620d41ede300d3143d4a'
 end
